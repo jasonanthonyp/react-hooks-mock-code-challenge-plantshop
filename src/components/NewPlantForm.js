23031677b74b50934e4e0dfe0f8b5125f2a855c9
Jason Phillips <jasonanthonyp@gmail.com>
2023-10-11 14:18:22 -0400

    finished

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ newPlants, onChangePlants, onAddPlant }) {
+  
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={onAddPlant}>
+        <input type="text" name="name" placeholder="Plant name" value={newPlants.name} onChange={onChangePlants}/>
+        <input type="text" name="image" placeholder="Image URL" value={newPlants.image} onChange={onChangePlants}/>
+        <input type="number" name="price" step="0.01" placeholder="Price" value={newPlants.price} onChange={onChangePlants}/>
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+  const [newPlants, setNewPlants] = useState({
+    name: "",
+    image: "",
+    price: 0
+  })
+
+  const [search, setSearch ] = useState("")
+
+ // const filteredPlants = plants.filter(plant => plant.name.toLowerCase().includes(search.toLowerCase()))
+ // this line is breaking everything SOS
+
+    
+
+  useEffect(() => {
+      fetch("http://localhost:6001/plants")
+    .then((r) => r.json())
+    .then(setPlants)
+  }, [])
+
+  function onChangePlants(e) {
+    setNewPlants({...newPlants, [e.target.name] : e.target.value })
+  }
+
+  function onAddPlant(e) {
+    e.preventDefault()
+
+    fetch('http://localhost:6001/plants', {
+      method: 'POST',
+      headers: {
+          'Content-Type':'application/json'
+      },
+      body: JSON.stringify(newPlants)
+      })
+      .then(res => res.json())
+      .then(newlyAddedPlant => setNewPlants({...plants, newlyAddedPlant}))
+    }
+
+  
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={onAddPlant} newPlants={newPlants} onChangePlants={onChangePlants}/>
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plants={plants} setPlants={setPlants} />
     </main>
   );
 }
